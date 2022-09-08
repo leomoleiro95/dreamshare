72de75fe2a9d457aa7a7a5cf3c021fc718534693
leomoleiro95 <leomoleiro@gmail.com>
2022-09-07 19:50:39 -0500

    Updates

---
+++ precache-manifest.23c7b4acfb8cd243befcbf50ceaafb39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f7fc169666e2869d477c657e1e29673",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c070164b0ccbb532026",
+    "url": "/static/css/main.2f765595.chunk.css"
+  },
+  {
+    "revision": "c2e49c6728381ccd7f54",
+    "url": "/static/js/2.1ff3efbf.chunk.js"
+  },
+  {
+    "revision": "1a1803a22b03bb07b77d827b47f236d9",
+    "url": "/static/js/2.1ff3efbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c070164b0ccbb532026",
+    "url": "/static/js/main.7d73ee4e.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.7d73ee4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e34072384d4efa27441f",
+    "url": "/static/js/runtime-main.18ea5ff7.js"
+  }
+]);
